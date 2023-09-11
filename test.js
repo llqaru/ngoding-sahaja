0a58578e8e811631bd812d4d40c35cd73f47d284
llqaru <loxicverse@gmail.com>
2023-09-11 13:56:49 +0700

    reupload

---
+++ test.js
@@ -0,0 +1,39 @@
+console.log("Hello World");
+console.log("Random Para Nolep");
+
+
+// belajar algoritma
+
+/*
+
+cara memasukan uang ke atm
+misal atm hanya bisa menerima uang maks 1 jt
+lalu kita mau memasukan uang 1,5 jt
+
+langkah awal
+kita masukan uang 1 jt sekaligus ke mesin atm
+jika kita masukan uang 1,5 jt maka akan ada peringatan
+
+lalu kalau sudah memasukan uang 1 jt atau kurang dari itu
+uang yang ada dikurangi uang yang masuk ke mesin atm
+
+lakukan hal tersebut sampai semua uang masuk ke mesin atm
+
+*/
+
+/*
+
+cara menghitung kembalian
+misal barang yang kita beli harganya 35 rb
+lalu kita mempunyai uang 50 rb
+
+langkah awal
+kita bayar dengan uang 50 rb
+uang yang dibayarkan melebihi harga barang
+
+lalu kita kurangi uang untuk bayar dengan harga barang
+hasil yang tersisa menjadi uang kembalian
+
+lakukan hal tersebut sampai tidak ada kembalian
+*/
+// 
